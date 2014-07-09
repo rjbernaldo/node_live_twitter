ea59625d3e15f3039c61d8ff0d7c582ae95d8573
Rj <rj.bernaldo@gmail.com>
2014-07-09 14:04:55 -0700

    Removed auth keys, removed jquery

--- app.js
+++ app.js
@@ -4,20 +4,16 @@ var express = require('express')
   , io      = require('socket.io').listen(server)
   , util    = require('util')
   , twitter = require('twitter')
+  , env     = require('node-env-file')
   , search  = ""
-  , t       = new twitter({
-    consumer_key: 'VdyhoULpWGim402KmkQFUEkNQ',
-    consumer_secret: 'ennIezQrxbteRVTE4ScrkA2t70X3o5G7HszOs0lZ1uahyBuZH2',
-    access_token_key: '2606164616-4ubLDi2559If7jtJT2qYg0jFdpKlPL5ACCS33Gz',
-    access_token_secret: 'ROGpwwJ3zB5sRezh42kVaa5xvWfsNgOEBh9nhrfGVehod'
-  });
+  , t;
 
 // Welcome message
 server.listen(3000);
 console.log("Node server started. Listening on port: 3000")
 
 // Public folder
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 // Routes
 app.get('/', function(req,res) {
@@ -25,24 +21,32 @@ app.get('/', function(req,res) {
   search = req.query || ""
 });
 
+// Twitter login
+env(__dirname + '/.env')
+t = new twitter({
+  consumer_key: process.env.consumerkey,
+  consumer_secret: process.env.consumersecret,
+  access_token_key: process.env.accesstokenkey,
+  access_token_secret: process.env.accesstokensecret
+});
+
 // Twitter streaming
 io.sockets.on('connection', function() {
-  io.sockets.emit('new message', "WELCOME!")
-  if (search == "") {
-    t.stream('statuses/sample', function(stream) {
+  // if (search == "") {
+    // t.stream('statuses/sample', function(stream) {
+    //   stream.on('data', function(data){
+    //     if (data.id != null) {
+    //       io.sockets.emit('new message', data);
+    //     }
+    //   });
+    // });
+  // } else {
+    t.stream('filter', { track: "food" }, function(stream) {
       stream.on('data', function(data){
         if (data.id != null) {
           io.sockets.emit('new message', data);
         }
       });
     });
-  } else {
-    t.stream('filter', { track: search.query }, function(stream) {
-      stream.on('data', function(data){
-        if (data.id != null) {
-          io.sockets.emit('new message', data);
-        }
-      });
-    });
-  }
+  // }
 });
--- public/web.js
+++ public/web.js
@@ -1,41 +1,64 @@
-jQuery(function($) {
+// ALL LOGIC ONLOAD
+window.onload = function() {
   var w      = document.body.clientWidth
     , h      = document.body.clientHeight
-    , socket = io.connect();
+    , socket = io.connect()
+    , tweets = document.getElementById('tweets')
+    , filter  = document.getElementById('filter');
+
+  socket.on('connect', function() {
+    console.log('User connected to tweet stream!');
+  })
 
   socket.on('new message', function(data) {
-    var photo_div = document.createElement('section');
-    photo_div.className = "tweet_photo";
+    // if (data.text.toLowerCase().indexOf(filter.value) >= 0) {
+      var tweet_div = createDiv('tweet_tweet')
+        , photo_div = createDiv('tweet_photo')
+        , text_div = createDiv('tweet_text')
+        , main_div = createDiv('tweet_main');
 
-    var text_div = document.createElement('section');
-    text_div.className = "tweet_text";
-
-    if (data.user != undefined) {
-      photo_div.style.backgroundImage = "url(" + data.user.profile_image_url + ")";
-      text_div.innerHTML = data.user.name + "<br>" + data.text;
-    }
-
-    var main_div = document.createElement('div');
-    main_div.className = "tweet_main";
-    main_div.style.left = Math.floor(Math.random(w) * 1000);
-    main_div.style.top = Math.floor(Math.random(h) * 1000);
-    main_div.appendChild(text_div);
-    main_div.appendChild(photo_div);
-
-    $('#tweets').append(main_div);
-    setTimeout(function() {
-      // fadeTweet(main_div);
-    }, Math.floor(Math.random() * 10000));
-
-    function fadeTweet(tweet, opacity) {
-      opacity = opacity || 0;
-      if (opacity <= 100) {
-        tweet.style.opacity = 1 - opacity/100;
-        setTimeout(function() {
-          fadeTweet(tweet, opacity + 5);
-        }, 0);
+      if (data.user != undefined) {
+        tweet_div.innerHTML = data.text;
+        photo_div.style.backgroundImage = "url(" + data.user.profile_image_url + ")";
+        text_div.innerHTML = data.user.name;
       }
-    }
 
+      main_div.style.left = Math.floor(Math.random(w) * 1000);
+      main_div.style.top = Math.floor(Math.random(h/2 ) * 500);
+
+      massAppend(main_div, [tweet_div, text_div, photo_div]);
+
+      tweets.appendChild(main_div);
+
+      setTimeout(function() {
+        fadeTweet(tweets, main_div);
+      }, 2000);
+    // }
   })
-})
+}
+
+// HELPERS
+function createDiv(className) {
+  var div = document.createElement('div');
+  div.className = className;
+  return div
+}
+
+function massAppend(div, arr) {
+  arr.forEach(function(elem) {
+    div.appendChild(elem);
+  })
+}
+
+function fadeTweet(tweets, tweet, opacity) {
+  opacity = opacity || 0;
+  if (opacity <= 100) {
+    tweet.style.opacity = 1 - opacity/100;
+    setTimeout(function() {
+      fadeTweet(tweets, tweet, opacity + 1);
+    }, 0);
+  } else {
+    tweet.style.opacity = 0;
+    tweets.removeChild(tweet);
+  }
+}
